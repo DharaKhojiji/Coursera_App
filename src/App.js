6aa6b1cb90490632d3d5b8664d9ee12f13dcf4b5
dhara khojiji <dharaktridhyatech@gmail.com>
2022-03-23 19:05:37 +0530

    dish details display correct

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import Menu from "./Components/Menu";
 import MenuDetails from "./Components/MenuDetails";
 import Navbar from "./Components/Navbar";
+import { DISHES } from "./Components/Shared/Dishes";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Menu />
+      {/* {DISHES.map((item) => ( */}
+      <div className="container">
+        <Menu datadish={DISHES}></Menu>
+      </div>
+      {/* ))} */}
+      {/* <Menu /> */}
     </div>
   );
 }
